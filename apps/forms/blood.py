4dafbdf4c9983bb90030ad839eeaaf161d6897b3
HolyFrancis <holyfrancisdushime@gmail.com>
2023-12-04 15:49:43 +0200

    added the blood form

---
+++ apps/forms/blood.py
@@ -0,0 +1,31 @@
+from django.forms import ModelForm
+
+from apps.models import Blood
+
+
+class BloodForm(ModelForm):
+    class Meta:
+        model = Blood
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["serial"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Serial"}
+        )
+        self.fields["volume"].widget.attrs.update(
+            {
+                "class": "form-select",
+            }
+        )
+        self.fields["sample"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Sample"}
+        )
+        self.fields["anylised"].widget.attrs.update(
+            {
+                "class": "form-check-input",
+            }
+        )
+        self.fields["state"].widget.attrs.update({"class": "form-select"})
+        self.fields["centrifuged"].widget.attrs.update({"class": "form-ckeck-input"})
+        self.fields["donor"].widget.attrs.update({"class": "form-select"})
