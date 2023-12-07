53324c83ac779b6addefe12d892d65435335294a
HolyFrancis <holyfrancisdushime@gmail.com>
2023-12-07 09:51:56 +0200

    added the __str__ to models

--- apps/models/blood.py
+++ apps/models/blood.py
@@ -2,15 +2,23 @@ from django.db import models
 
 from apps.models.donor import Donor
 
+
 class Blood(models.Model):
-    STATE = (("Eligible","Eligible"), ("Ineligible","Ineligible"), ("Attente","Analyse en Cours"))
-    VOLUME = (("350","350"), ("400","400"),("450","450"),("500","500"))
+    STATE = (
+        ("Eligible", "Eligible"),
+        ("Ineligible", "Ineligible"),
+        ("Attente", "Analyse en Cours"),
+    )
+    VOLUME = (("350", "350"), ("400", "400"), ("450", "450"), ("500", "500"))
     serial = models.CharField(max_length=255, blank=False, null=False)
     volume = models.FloatField(null=False, blank=False, choices=VOLUME)
-    sample = models.CharField(max_length=255,null=False, blank=False)
+    sample = models.CharField(max_length=255, null=False, blank=False)
     analysed = models.BooleanField(default=False)
-    state = models.CharField(max_length=255,choices=STATE)
+    state = models.CharField(max_length=255, choices=STATE)
     centrifuged = models.BooleanField(default=False)
-    donor = models.ForeignKey(Donor, on_delete=models.CASCADE) 
+    donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.serial} -- {self.volume}"
