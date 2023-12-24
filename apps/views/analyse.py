8d35225825769527e2d1ca841f2bcf0a10a708de
HolyFrancis <holyfrancisdushime@gmail.com>
2023-12-24 15:13:48 +0200

    bug fix

--- apps/views/analyse.py
+++ apps/views/analyse.py
@@ -10,89 +10,101 @@ def analyse(request):
     analyses = Analysis.objects.all()
     filtre = AnalysisFilter(request.GET, queryset=analyses)
     analyses = filtre.qs
-    
-    context = {'analyses':analyses, "filtre":filtre}
-    
+
+    context = {"analyses": analyses, "filtre": filtre}
+
     return render(request, "apps/analyse/analyse.html", context)
 
 
 def create_analysis(request, id):
     blood = Blood.objects.get(id=id)
-    form = AnalysisForm(initial={'blood':blood})
-    
+    form = AnalysisForm(initial={"blood": blood})
+
     if request.method == "POST":
         form = AnalysisForm(request.POST)
         if form.is_valid():
             analysis = form.save(commit=False)
-            
-            if analysis.irregular_antibodies == 'Négatif' and analysis.hiv_test == 'Négatif' and analysis.hepatites_test == 'Négatif' and analysis.anti_hlv1_test == 'Négatif' and analysis.anti_hlv2_test == 'Négatif' and analysis.malaria_test == 'Négatif':
-                analysis.result = 'Négatif'
+
+            if (
+                analysis.irregular_antibodies == "Négatif"
+                and analysis.hiv_test == "Négatif"
+                and analysis.hepatites_test == "Négatif"
+                and analysis.anti_hlv1_test == "Négatif"
+                and analysis.anti_hlv2_test == "Négatif"
+                and analysis.malaria_test == "Négatif"
+            ):
+                analysis.result = "Négatif"
             else:
-                analysis.result = 'Positif'
-                
+                analysis.result = "Positif"
+
             analysis.save()
-            
-            if analysis.result=='Négatif':
-                blood.state = 'Eligible'
-            elif analysis.result=="Positif":
-                blood.state = 'Ineligible'
-                
+
+            if analysis.result == "Négatif":
+                blood.state = "Eligible"
+            elif analysis.result == "Positif":
+                blood.state = "Ineligible"
+
             blood.analysed = True
             blood.save()
-            item = request.POST
+            # item = request.POST
             messages.success(request, "Analyse pour la " + str(blood) + " créée avec succès")
-            return redirect('request_analysis')
+            return redirect("request_analysis")
         else:
             print(form.errors)
-    
-    context = {"form":form, "blood":blood}
-    
+
+    context = {"form": form, "blood": blood}
+
     return render(request, "apps/analyse/create_analyse.html", context)
+
 
 def update_analysis(request, id):
     analysis = Analysis.objects.get(id=id)
-    blood = Blood.objects.get(id=analysis.blood)
-    
+    blood = Blood.objects.get(id=analysis.blood.id)
+
     form = AnalysisForm(instance=analysis)
-    
+
     if request.method == "POST":
         form = AnalysisForm(request.POST, instance=analysis)
         if form.is_valid():
             form.save()
-            item = request.POST
+            # item = request.POST
             messages.success(request, "Analyse pour la " + str(blood) + " modifiée avec succès")
-            return redirect('analyse')
+            return redirect("analyse")
         else:
             return form.errors
-    
-    context = {"form":form, 'analysis':analysis}
-    
+
+    context = {"form": form, "analysis": analysis}
+
     return render(request, "apps/analyse/create_analyse.html", context)
+
 
 def analysis_details(request, id):
     analysis = Analysis.objects.get(id=id)
-    
-    context = {'analysis':analysis}
-    
+
+    context = {"analysis": analysis}
+
     return render(request, "apps/analyse/details.html", context)
+
 
 def delete_analysis(request, id):
     analysis = Analysis.objects.get(id=id)
     analysis.delete()
     messages.success(request, "Analyse supprimée avec succès")
-    
-    return redirect('analyse')
+
+    return redirect("analyse")
+
 
 def request_analysis(request):
     bloods = Blood.objects.filter(analysed=False)
-    
-    context = {'bloods':bloods}
-    
+
+    context = {"bloods": bloods}
+
     return render(request, "apps/analyse/requests.html", context)
+
 
 def analysis_history(request):
     analyses = Analysis.objects.all()
-    
-    context = {'analyses':analyses}
-    
+
+    context = {"analyses": analyses}
+
     return render(request, "apps/analyse/history.html", context)
