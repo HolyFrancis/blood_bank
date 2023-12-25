15491cd2f56a9270a58ab99700d0af19f5e56ed0
Ahsan Jamiyl <jamiylnkunda@gmail.com>
2023-12-25 13:35:25 +0200

    relationship of user and client

--- apps/models/client.py
+++ apps/models/client.py
@@ -1,12 +1,17 @@
 from django.db import models
 
+from apps.models import Users
+
 class Client(models.Model):
+    user = models.OneToOneField(Users, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null = False, blank=False)
     phone = models.CharField(max_length=50, null = False, blank=False)
     email = models.CharField(max_length=50, null = False, blank=False)
     adress = models.CharField(max_length=255, null = False, blank=False)
+    password = models.CharField(max_length=50)
     date_created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.name
