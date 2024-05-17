03df37fb476a97c8e014dbe755780f629f916881
HolyFrancis <holyfrancisdushimegmail.com>
2024-05-17 19:26:49 +0200

    used donor's username and password to create donor user

--- apps/forms/donor.py
+++ apps/forms/donor.py
@@ -2,32 +2,35 @@ from django.forms import ModelForm
 
 from apps.models.donor import Donor
 
+
 class DonorForm(ModelForm):
     class Meta:
         model = Donor
-        fields = '__all__'
-        
+        fields = "__all__"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs.update({'class':'form-control', 'placeholder':'Nom'})
-        self.fields['last_name'].widget.attrs.update({'class':'form-control', 'placeholder':'Prénom(s)'})
-        self.fields['cni'].widget.attrs.update({'class':'form-control', 'placeholder':'CNI'})
-        self.fields['weight'].widget.attrs.update({'class':'form-control', 'placeholder':'Poids'})
-        self.fields['sex'].widget.attrs.update({'class':'form-select'})
-        self.fields['blood_group'].widget.attrs.update({'class':'form-select'})
-        self.fields['last_donation'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['drugs'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['tattoo_piercing'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['disease'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['surgery'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['under_medication'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['vaccine'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['dentist'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['pregnancy'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['transfusion'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['anemia'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['infections'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['examens'].widget.attrs.update({'class':'form-check-input'})
-        self.fields['email'].widget.attrs.update({'class':'form-control'})
-        self.fields['phone'].widget.attrs.update({'class':'form-control', 'placeholder':'Numéro de Téléphone'})
-        self.fields['status'].widget.attrs.update({'class':'form-select', 'placeholder':'Adresse mail'})
+        self.fields["first_name"].widget.attrs.update({"class": "form-control", "placeholder": "Nom"})
+        self.fields["last_name"].widget.attrs.update({"class": "form-control", "placeholder": "Prénom(s)"})
+        self.fields["cni"].widget.attrs.update({"class": "form-control", "placeholder": "CNI"})
+        self.fields["weight"].widget.attrs.update({"class": "form-control", "placeholder": "Poids"})
+        self.fields["sex"].widget.attrs.update({"class": "form-select"})
+        self.fields["blood_group"].widget.attrs.update({"class": "form-select"})
+        self.fields["last_donation"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["drugs"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["tattoo_piercing"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["disease"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["surgery"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["under_medication"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["vaccine"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["dentist"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["pregnancy"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["transfusion"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["anemia"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["infections"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["examens"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["email"].widget.attrs.update({"class": "form-control", "placeholder": "Adresse mail"})
+        self.fields["phone"].widget.attrs.update({"class": "form-control", "placeholder": "Numéro de Téléphone"})
+        self.fields["status"].widget.attrs.update({"class": "form-select"})
+        self.fields["username"].widget.attrs.update({"class": "form-control", "placeholder": "Nom d'utilisateur"})
+        self.fields["password"].widget.attrs.update({"class": "form-control", "placeholder": "Votre mot de passe"})
--- apps/views/public.py
+++ apps/views/public.py
@@ -2,25 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from apps.forms import DonorForm
-from apps.models import Donor
+from apps.models import Donor, Users
+
 
 def public(request):
-     
+
     form = DonorForm()
     if request.method == "POST":
         form = DonorForm(request.POST)
+        print(f"FORM IS VALID {form.is_valid()}")
         if form.is_valid():
-            form.save()
-            donor = request.POST
+            donor = form.save()
+            # donor = request.POST
+            user = Users.objects.create(username=form.cleaned_data["username"], email=form.cleaned_data["email"], first_name=form.cleaned_data["first_name"], last_name=form.cleaned_data["last_name"])
+            print(f"USER ===={user}")
+            user.set_password(form.cleaned_data["password"])
+            user.donor = donor
+            user.save()
             messages.success(request, "Vous êtes désormais inscrit en tant que donneur potentiel. Votre requête sera étudiée. Vous recevrez plus d'instructions par email ou par messagerie")
-            return redirect('home')
+            return redirect("login")
         else:
             donor = request.POST
             donors = Donor.objects.all()
             for don in donors:
-                if don.cni == donor['cni']:
+                if don.cni == donor["cni"]:
                     messages.error(request, "Un donneur avec la CNI saisie saisie existe déjà!")
-    
-    context = {'form':form}
-    
+
+    context = {"form": form}
+
     return render(request, "apps/public/index.html", context)
