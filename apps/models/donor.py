29940d030e7438539072371bda3112f1e0e9cfc1
HolyFrancis <holyfrancisdushimegmail.com>
2024-05-17 19:20:59 +0200

    added relation between user and donor

--- apps/models/donor.py
+++ apps/models/donor.py
@@ -43,6 +43,8 @@ class Donor(models.Model):
     status = models.CharField(null=True, blank=True, choices=STATUS, default=STATUS[1], max_length=50)
     date_created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    password = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- apps/models/user.py
+++ apps/models/user.py
@@ -3,13 +3,14 @@ from django.db import models
 
 
 class Users(AbstractUser):
-    ROLES = (
-        ('Admin','Admin'), 
-        ('Client','Client'),
-        ('Docteur','Docteur'),
-        ('Gestionnaire','Gestionnaire'),
-        ('Laborantin','Laborantin'),
-        ('Infirmier(e)','Infirmier(e)')
-        )
+    ROLES = (("Admin", "Admin"), ("Client", "Client"), ("Docteur", "Docteur"), ("Gestionnaire", "Gestionnaire"), ("Laborantin", "Laborantin"), ("Infirmier(e)", "Infirmier(e)"))
     phone_number = models.CharField(max_length=50, null=True, blank=True)
     role = models.CharField(max_length=20, null=True, blank=False, choices=ROLES)
+    donor = models.ForeignKey("Donor", on_delete=models.CASCADE, null=True, blank=True, related_name="users", related_query_name="user")
+
+    @property
+    def is_donor(self):
+        if self.donor:
+            return True
+        else:
+            return False
